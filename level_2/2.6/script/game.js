91897b1f3d0a5bde9dc36b49f94c8728427de4f4
casswitch2319 <cassandre_fed@yahoo.com>
2021-04-30 09:59:20 -0400

    2.6

---
+++ level_2/2.6/script/game.js
@@ -0,0 +1,179 @@
+var c = document.querySelector('canvas');
+var ctx = c.getContext('2d')
+var interval = 1000 / 60
+var timer
+var ball  
+var paddle
+//var gravity = .05
+//var gravitySpeed=.05
+
+
+     //Define Booleans for each key
+     var a = false;
+     var d = false;
+     var s = false;
+     var w = false;
+     
+
+         
+//game objects
+ball = new GameObject()
+paddle = new GameObject()
+
+ball.leftSide = function(){return ball.x - ball.width/2};
+ball.rightSide = function(){return ball.x + ball.width/2};
+ball.topSide = function(){return ball.y - ball.height/2};
+ball.bottomSide = function(){return ball.y + ball.height/2};
+
+paddle.leftSide = function(){return paddle.x - paddle.width/2};
+paddle.rightSide = function(){return paddle.x + paddle.width/2};
+paddle.topSide = function(){return paddle.y - paddle.height/2};
+paddle.bottomSide = function(){return paddle.y + paddle.height/2};
+
+//velocity
+    ball.vx = 10;
+    ball.vy = 0;
+//paddle location
+    paddle.x= paddle.width/2
+    paddle.y= c.height/2
+    paddle.width = 10
+    paddle.height = 150
+    
+ 
+
+    timer = setInterval(animate, interval);
+
+    //game movements
+function animate() {
+
+ctx.clearRect(0,0, canvas.width, canvas.height)
+
+ball.moveCircle()
+paddle.moveRect()
+
+//boundaries for ball
+//bottom
+if(ball.y>canvas.height-ball.height/2)
+{
+    ball.y = canvas.height -ball.height/2
+    ball.vy= -ball.vy
+    ball.bottomSide = function(){return ball.y + ball.height/2};
+}
+
+//top
+if(ball.y  <ball.height/2)
+{
+    ball.y = ball.height/2
+    ball.vy= -ball.vy
+    ball.topSide = function(){return ball.y - ball.height/2};
+}
+//left
+if(ball.x< ball.width/2)
+{
+    ball.x = canvas.width/2
+    ball.y = canvas/height/2
+    ball.vx= ball.vx
+    ball.leftSide = function(){return ball.x - ball.width/2};
+}
+
+//right
+if(ball.x  > canvas.width - ball.width/2 )
+{
+    ball.x = canvas.width - ball.width/2
+    ball.vx= -ball.vx
+    ball.rightSide = function(){return ball.x + ball.width/2};
+}
+
+//boundaries for paddle
+if(paddle.y>canvas.height - paddle.height/2){
+    paddle.y=canvas.height- paddle.height/2
+
+
+}
+if(paddle.y<paddle.height/2){
+   paddle.y = paddle.height/2
+} 
+
+
+     //Add Event Listeners
+     document.addEventListener("keydown", press);
+     document.addEventListener("keyup", release);
+     
+     //Event Functions
+     function press(e)
+     {
+         //---This logs key codes into the browser's console.
+         //console.log("Pressed" + e.keyCode);
+         /*
+         //left
+         if(e.keyCode == 65)
+         {
+             a = true;
+         }
+        //right
+         if(e.keyCode == 68)
+         {
+             d = true;
+         }*/
+         //up
+         if(e.keyCode == 87)
+         {
+             w = true;
+         }
+         //down
+         if(e.keyCode == 83)
+         {
+             s = true;
+         }
+
+     }
+     
+     function release(e)
+     {
+         //---This logs key codes into the browser's console.
+         //console.log("Released" + e.keyCode);
+         
+         if(e.keyCode == 65)
+         {
+             a = false;
+         }
+         if(e.keyCode == 68)
+         {
+             d = false;
+         }
+         if(e.keyCode == 87)
+         {
+             w = false;
+         }
+         if(e.keyCode == 83)
+         {
+             s = false;
+         }
+     }
+     
+     function hitTestObject(ball, paddle)
+     {
+      if( ball.leftSide() < paddle.rightSide() &&
+         ball.rightSide() > paddle.leftSide() &&
+         ball.topSide() < paddle.bottomSide() &&
+         ball.bottomSide() > paddle.topSide() )
+         {
+           return true;
+         }
+      return false;
+     }
+     if(hitTestObject(ball, paddle))
+     {
+       //paddle.color = "yellow"
+    
+       ball.vx = -ball.vx
+
+
+     }
+
+ball.drawCircle()
+paddle.drawRect()
+console.log(paddle.y)
+
+
+}
---
+++ level_2/2.6/script/gameObject.js
@@ -0,0 +1,111 @@
+
+
+function GameObject() {
+
+   
+    //location
+    this.x = canvas.width / 2
+    this.y = canvas.height / 2
+    //size
+    this.width = 50
+    this.height = 50
+
+
+    //player's color
+    this.color = "aquamarine";
+
+
+    //player's velocity or speed on each axis
+    this.vx = 0
+    this.vy = 0
+
+    //gravity
+   // this.gravity=0
+
+   //draws circle to canvas
+    this.drawCircle = function () {
+        ctx.save();
+        ctx.fillStyle = "purple";
+        ctx.beginPath();
+        ctx.translate(this.x, this.y);
+        ctx.arc(0, 0, this.width / 2, 0, 360 * Math.PI / 180, true);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+
+    }
+//draws rectangle
+    this.drawRect = function (){
+
+        ctx.save();
+        ctx.fillStyle = this.color;
+        ctx.translate(this.x, this.y);
+        ctx.fillRect(-this.width/2,-this.height/2,this.width,this.height);
+        ctx.restore();
+        
+    }
+
+    //This changes the player's position
+    this.moveCircle = function () {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    this.moveRect = function(){
+       	//Move the Player to the right
+	if(d)
+	{
+		console.log("Moving Right");
+		paddle.x += 3;
+	}
+	if(a)
+	{
+		console.log("Moving Left");
+		paddle.x += -3;
+	}
+	if(w)
+	{
+		console.log("Moving Up");
+		paddle.y += -3;
+	}
+	if(s)
+	{
+		console.log("Moving Down");
+		paddle.y += 3;
+	}
+       
+    this.left = function() 
+	{
+		return this.x - this.width/2;
+	}
+	this.right = function() 
+	{
+		return this.x + this.width/2;
+	}
+	
+	this.top = function() 
+	{
+		return this.y - this.height/2;
+	}
+	this.bottom = function() 
+	{
+		return this.y + this.height/2;
+	}
+	
+	this.hitTestObject = function(obj)
+	{
+		if(this.left() < obj.right() && 
+		   this.right() > obj.left() &&
+		   this.top() < obj.bottom() &&
+		   this.bottom() > obj.top())
+		{
+			return true
+		}
+		return false;
+	}
+       
+       
+   
+    
+    }
+}
